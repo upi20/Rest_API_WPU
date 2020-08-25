e279490edd91e6ec65d788832cbf57f77decddb8
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-08-25 10:35:56 +0700

    fix

--- 03 Apa Itu JSON/latihan3 decode.php
+++ 03 Apa Itu JSON/latihan3 decode.php
@@ -2,5 +2,6 @@
 
 $data = file_get_contents('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
 $mahasiswa = json_decode($data, true);
-var_dump($mahasiswa);
+$data = json_encode($mahasiswa);
+echo $data;
 
