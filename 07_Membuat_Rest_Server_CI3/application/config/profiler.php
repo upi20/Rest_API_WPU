e358c7111765ad2f864e7d2fc4414d4cac1acdff
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-08-29 10:23:17 +0700

    RestServer added

--- 07_Membuat_Rest_Server_CI3/application/config/profiler.php
+++ 07_Membuat_Rest_Server_CI3/application/config/profiler.php
@@ -9,6 +9,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | data are displayed when the Profiler is enabled.
 | Please see the user guide for info:
 |
-|	https://codeigniter.com/user_guide/general/profiling.html
+|	http://codeigniter.com/user_guide/general/profiling.html
 |
 */
+$config['benchmarks']           = true;
+$config['config']               = true;
+$config['controller_info']      = true;
+$config['get']                  = true;
+$config['http_headers']         = true;
+$config['memory_usage']         = true;
+$config['post']                 = true;
+$config['queries']              = true;
+$config['eloquent']             = false;
+$config['uri_string']           = true;
+$config['view_data']            = true;
+$config['query_toggle_count']   = 1000;
